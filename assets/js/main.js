2373ec1fd955c6913f31b9422372257ccb8438bc
Erika Navarro <erika072002@gmail.com>
2023-11-15 10:09:09 -0500

    localStorage implemented (11/15/2023)
    Can retrieve values inputed by user on form and use that to store (key,value) pairs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,36 @@
 //Retrieving Data from form
 let fname = document.querySelector("#fname").value;
 let lname = document.querySelector("#lname").value;
-console.log(fname);
-// let userAge = document.querySelector("#age").value;
+let userAge = document.querySelector("#age").value;
 
-// store the data
-let userName = localStorage.setItem('fname',fname);
-//console.log(userName); //-> "Erika"
+
+//Defining the form being used
+myForm.addEventListener("submit", (event) => {
+
+    // prevent default behavior of form
+	event.preventDefault();
+
+    // create variables for all items
+	let fname = this.fname.value;
+	let lname = this.lname.value;
+    let userAge = this.age.value;
+
+    localStorage.setItem('fname',fname);
+    let userFirstName = localStorage.getItem("fname");
+    console.log(userFirstName);
+
+
+    localStorage.setItem('lname',lname);
+    let userLastName = localStorage.getItem("lname");
+    console.log(userLastName);
+
+    localStorage.setItem('userAge',userAge);
+    let age = localStorage.getItem("userAge");
+    console.log(age);
+});
+
+
+
+
 
 
