68e12e9edae3ae85c5f6a5e92545b02c0484b911
Erika Navarro <erika072002@gmail.com>
2023-11-16 07:53:26 -0500

    Added localStorage example from class (11/16/2023)

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,6 +37,32 @@ myForm.addEventListener("submit", (event) => {
 
 });
 
+// // save primitive value
+// localStorage.setItem("_a", 123);
+
+// // example object 
+// let tasks = [
+// 	{ name: "Replace plastic water bottle use with a reusable water bottle", status: false },
+// 	{ name: "Bike or walk instead of drive somewhere", status: true },
+// 	{ name: "Recycle any material you use", status: false }
+// ];
+// showTasks(tasks);
+
+// // serialize and save an object 
+// localStorage.setItem("_tasks", JSON.stringify(tasks));
+// // get and deserialize the object
+// let newTasks = JSON.parse(localStorage.getItem("_tasks"));
+// showTasks(newTasks);
+
+// function showTasks(data) {
+// 	let str = "";
+// 	for (let i = 0; i < tasks.length; i++) {
+// 		str += data[i].name + " - " + data[i].status + "<br>";
+// 	}
+// 	document.body.innerHTML += str;
+// }
+
+
 
 
 
