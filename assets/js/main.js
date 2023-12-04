d5368d63725acd76ceb9c1ebebc2f366d1d6be21
Erika Navarro <erika072002@gmail.com>
2023-12-04 15:34:56 -0500

    Implemented check boxes and created master task list (12/04/2023)

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,35 +5,49 @@ let fname = document.querySelector("#fname").value;
 
 //Defining the form being used
 myForm.addEventListener("submit", (event) => {
+  // prevent default behavior of form
+  event.preventDefault();
 
-    // prevent default behavior of form
-	event.preventDefault();
-
-    // create variables for all items
-	let fname = this.fname.value;
-	localStorage.setItem("fname", fname);
-    document.querySelector(".fname").innerHTML = localStorage.getItem("fname");
+  // create variables for all items
+  let fname = this.fname.value;
+  localStorage.setItem("fname", fname);
+  document.querySelector(".fname").innerHTML = localStorage.getItem("fname");
 });
 
-
-// example object (use this example to set up tasks page) 
+// example object (use this example to set up tasks page)
 let taskList = [
-	{ name: "Replace plastic water bottle use with a reusable water bottle", status: false },
-	{ name: "Bike or walk instead of drive somewhere", status: true },
-	{ name: "Recycle any material you use", status: false }
+  {
+    name: "Replace plastic water bottle use with a reusable water bottle",
+    status: false,
+    id: 1,
+  },
+  {
+    name: "Bike or walk instead of drive somewhere",
+    status: true,
+    id: 2,
+  },
+  { name: "Recycle any material you use", 
+  status: false, 
+  id: 3 
+  },
 ];
 showTasks(".taskList", taskList);
 
-// serialize and save an object 
+// serialize and save an object
 localStorage.setItem("_taskList", JSON.stringify(taskList));
 // get and deserialize the object
 let newTasks = JSON.parse(localStorage.getItem("_taskList"));
 showTasks(".newTasks", newTasks);
 
 function showTasks(newTasks, data) {
-	let str = "";
-	for (let i = 0; i < taskList.length; i++) {
-		str += `<li>${data[i].name}</li>`;
-	}
-	document.querySelector(newTasks).innerHTML = str;
+  let str = "";
+  for (let i = 0; i < taskList.length; i++) {
+    str += `
+		<label class="container">${data[i].name}
+		<input type="checkbox" checked="checked" id="${data[i].id}">
+		<span class="checkmark"></span>
+	  	</label>
+		`;
+  }
+  document.querySelector(newTasks).innerHTML = str;
 }
---
+++ assets/js/tasks.js
@@ -0,0 +1,29 @@
+//Have an array that includes the id of the tasks already added in the taks list on the site
+let taskListMaster = [
+  {
+    name: "Replace plastic water bottle use with a reusable water bottle",
+    status: false,
+    id: 1,
+  },
+  {
+    name: "Bike or walk instead of drive somewhere",
+    status: true,
+    id: 2,
+  },
+  { 
+    name: "Recycle any material you use", 
+    status: false, 
+    id: 3 
+  },
+  {
+    name: "Create a compost bin for you to use in your home",
+    status: false,
+    id: 4
+  },
+  {
+    name: "Unplug any electronics not being used in your household",
+    status:false,
+    id:5
+  },
+
+];
