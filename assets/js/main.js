6c84a360573a5e384f91c6ce279680bbc391c0ee
Erika Navarro <erika072002@gmail.com>
2023-12-06 14:32:55 -0500

    Changes implemented during OH (12/06/2023)
    - Tasks now change after being checked off
    - New functions created to complete different actions needed to set/get/update tasks
    - Began setting up function that keeps score updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,52 +15,93 @@ myForm.addEventListener("submit", (event) => {
 });
 
 // example object (use this example to set up tasks page)
-let taskList = [
-  {
-    name: "Replace plastic water bottle use with a reusable water bottle",
-    status: false,
-    id: 1,
-    value: 5,
-  },
-  {
-    name: "Bike or walk instead of drive somewhere",
-    status: false,
-    id: 2,
-    value: 5,
-  },
-  { name: "Recycle any material you use", 
-    status: false, 
-    id: 3, 
-    value: 5
+let tasksCompleted = getTasks(); // array of ids
+let tasksToDisplay = []; // task objects from the master list
+console.log("tasksCompleted", tasksCompleted);
+
+async function updateTasksToDisplay() {
+  tasksCompleted = getTasks();
+  tasksToDisplay = [];
+
+  // first time visit
+  for (let i = 0; i < taskListMaster.length; i++) {
+    if (!tasksCompleted || !tasksCompleted.includes(taskListMaster[i].id)) {
+      tasksToDisplay.push(taskListMaster[i]);
+    }
+    console.log("updateTasksToDisplay()", tasksToDisplay);
+    // only allows 3
+    if (tasksToDisplay.length >= 3) break;
   }
-];
-showTasks(".taskList", taskList);
+}
+(async function () {
+  await updateTasksToDisplay();
+  showTasks();
+})();
 
-// serialize and save an object
-localStorage.setItem("_taskList", JSON.stringify(taskList));
-// get and deserialize the object
-let newTasks = JSON.parse(localStorage.getItem("_taskList"));
-showTasks(".newTasks", newTasks);
+// FUNCTIONS
 
-function showTasks(newTasks, data) {
-  let score = 0;
+function getTasks() {
+  // get and deserialize the object
+  return JSON.parse(localStorage.getItem("_tasksCompleted")) || [];
+}
+
+function setTasks(list) {
+  // serialize and save an object
+  localStorage.setItem("_tasksCompleted", JSON.stringify(list));
+}
+
+function showTasks() {
+  console.log("showTasks()", tasksToDisplay);
   let str = "";
-  for (let i = 0; i < taskList.length; i++) {
+  for (let i = 0; i < tasksToDisplay.length; i++) {
     //Check box code from W3 Schools
-    str += `
-		<label class="container">
-    <input type="checkbox" checked = false id="${data[i].id}">
-    ${data[i].name}
-	  </label>
-		`;
-
-    //Conditional to check if the checkbox is checked (make status true and increase score if checked)
-    if((data[i].status == false) && (checkbox.checked == checked)){
-      data[i].status = true;
-      score += data[i].value;
-      console.log(data[i].id);
-
-    };
+    let checked = "";
+    if (tasksCompleted.includes(tasksToDisplay[i].id)) {
+      checked = "checked";
+    }
+    str += `<label class="container">
+            <input type="checkbox" ${checked} data-id="${tasksToDisplay[i].id}" class="checkbox">
+            ${tasksToDisplay[i].name}
+            </label>
+            `;
   }
-  document.querySelector(newTasks).innerHTML = str;
+  document.querySelector(".taskList").innerHTML = str;
+}
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("checkbox")) {
+    // console.log(e.target.classList.contains("checkbox"));
+    //   console.log("a checkbox was clicked");
+    if (e.target.checked == true) {
+      //   console.log("a checkbox IS TRUE");
+
+      // get the id
+      let id = Number(e.target.dataset.id);
+      console.log(taskListMaster[id]);
+
+      if (tasksCompleted.includes(id)) {
+        return;
+      }
+
+      // update local
+      tasksCompleted.push(id);
+
+      // update localstorage
+      setTasks(tasksCompleted);
+
+      // update score
+      setScore(tasksCompleted);
+
+      setTimeout(async function () {
+        // update display
+        await updateTasksToDisplay();
+        showTasks();
+      }, 250);
+    }
+  }
+});
+
+let score = 0;
+function setScore(tasksCompleted) {
+  // for(let i=)
 }
--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,54 +1,69 @@
 //Have an array that includes the id of the tasks already added in the taks list on the site
 let taskListMaster = [
   {
+    name: "Start using this website",
+    status: false,
+    id: 0,
+    value: 5,
+  },
+  {
     name: "Replace plastic water bottle use with a reusable water bottle",
     status: false,
     id: 1,
+    value: 5,
   },
   {
     name: "Bike or walk instead of drive somewhere",
     status: true,
     id: 2,
+    value: 5,
   },
-  { 
-    name: "Recycle any material you use", 
-    status: false, 
-    id: 3 
+  {
+    name: "Recycle any material you use",
+    status: false,
+    id: 3,
+    value: 5,
   },
   {
     name: "Create a compost bin for you to use in your home",
     status: false,
-    id: 4
+    id: 4,
+    value: 5,
   },
   {
     name: "Unplug any electronics not being used in your household",
-    status:false,
-    id:5
+    status: false,
+    id: 5,
+    value: 5,
   },
   {
     name: "Take reusable bags to use when going to any store",
     status: false,
-    id:6
+    id: 6,
+    value: 5,
   },
   {
     name: "Shop for clothes at a thrift shop",
     status: false,
-    id: 7
+    id: 7,
+    value: 5,
   },
   {
     name: "Buy local produce",
     status: false,
-    id: 8
+    id: 8,
+    value: 5,
   },
   {
     name: "Carry your own utensils instead of using plastic ones",
     status: false,
-    id: 9 
+    id: 9,
+    value: 5,
   },
   {
     name: "Turn off the water when not in use",
     status: false,
-    id: 10 
-  }
-
+    id: 10,
+    value: 5,
+  },
 ];
